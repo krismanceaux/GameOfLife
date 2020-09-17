245cd0ae331049a38771eed7331cb94c01dc0f09
Kristopher Manceaux <krismanceaux@hotmail.com>
2020-09-16 23:48:21 -0500

    Added project metadata

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule Life.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description:
+        "Life is a lightweight package for Conway's Game of Life that takes in a set of live cells and returns the next generation. The new generation is stored as the state in a GenServer process in the GameServer module. This package leaves it up to the interface, whether it's the console or a web framework, to determine how to continue producing new generations and displaying them to the user. This package comes with a simple Console interface as an example. Instructions for using the console interface are in the README.",
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/krismanceaux/GameOfLife",
+        "GitLab" => "https://gitlab.com/kristophermanceaux/gameoflife"
+      },
+      maintainers: ["Kristopher Manceaux"],
+      source_url: "https://gitlab.com/kristophermanceaux/gameoflife"
     ]
   end
 
