d97cdaadad7076a2e3cec8fb5d9860d6c5ca1834
Kristopher Manceaux <krismanceaux@hotmail.com>
2020-09-17 00:00:49 -0500

    mix file

--- mix.exs
+++ mix.exs
@@ -9,14 +9,9 @@ defmodule Life.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description:
-        "Life is a lightweight package for Conway's Game of Life that takes in a set of live cells and returns the next generation. The new generation is stored as the state in a GenServer process in the GameServer module. This package leaves it up to the interface, whether it's the console or a web framework, to determine how to continue producing new generations and displaying them to the user. This package comes with a simple Console interface as an example. Instructions for using the console interface are in the README.",
-      licenses: ["MIT"],
-      links: %{
-        "GitHub" => "https://github.com/krismanceaux/GameOfLife",
-        "GitLab" => "https://gitlab.com/kristophermanceaux/gameoflife"
-      },
-      maintainers: ["Kristopher Manceaux"],
-      source_url: "https://gitlab.com/kristophermanceaux/gameoflife"
+        "Life is a lightweight package for Conway's Game of Life that takes in a set of live cells and returns the next generation.",
+      source_url: "https://gitlab.com/kristophermanceaux/gameoflife",
+      package: package()
     ]
   end
 
@@ -32,6 +27,17 @@ defmodule Life.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Kristopher Manceaux"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/krismanceaux/GameOfLife",
+        "GitLab" => "https://gitlab.com/kristophermanceaux/gameoflife"
+      }
     ]
   end
 end
